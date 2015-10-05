4b1d9e812f34a76a4245420855f85151b7ba0601
Gus42 <gus815@gmail.com>
2015-10-05 16:27:02 +0100

    data-bind the list of place

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-var model = {
-
-	parkList: [
+var ViewModel = function() {
+	this.parkList = ko.observableArray([
 		{"name": "Lenton Abbey Park", "location": "Derby Road, Lenton, Nottingham NG9 2SD", "lat": "52.9388484", "lng": "-1.2145866"},
 		{"name": "Highfields", "location": "University Boulevard, Lenton, Nottingham NG7 2RD", "lat": "52.9324746", "lng": "-1.1949166"},
 		{"name": "Victoria Embankment", "location": "Embankment Road, Nottingham NG2 2JY", "lat": "52.9349691", "lng": "-1.1463881"},
@@ -9,21 +8,22 @@ var model = {
 		{"name": "Arboretum Park", "location": "Waverley Street Nottingham NG7 4HF", "lat": "52.9613065", "lng": "-1.1571471"},
 		{"name": "Forest Recreation Ground", "location": "Gregory Boulevard Nottingham NG1 6HH", "lat": "52.9657282", "lng": "1.1602631"},
 		{"name": "Hungerhill Allotments", "location": "Ransom Rd Nottingham NG3 4JB", "lat": "52.9686424", "lng": "-1.1350619"}
-	],
-	init: function() {
+	]);
+	/*init: function() {
 
 	},
 	update: function() {
 
-	}
+	}*/
 }
-
+ko.applyBindings(new ViewModel());
+/*
 var view = {
 	init: function() {
 		var mapCanvas = document.getElementById('map');
 		var mapOptions = {
 			center: new google.maps.LatLng(52.9544367,-1.1585429),
-			zoom: 15,
+			zoom: 13,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		}
 		var map = new google.maps.Map(mapCanvas, mapOptions);
@@ -44,3 +44,4 @@ var control = {
 }
 
 control.init();
+*/
